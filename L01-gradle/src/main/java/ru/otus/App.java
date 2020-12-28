c5035ea95b487b1a83620fd2ae4dc4f8d40530ff
Aleksei Terentev <aleksei.terentev@NetCracker.com>
2020-12-28 17:03:42 +0400

    hw01-gradle

--- L01-gradle/src/main/java/ru/otus/App.java
+++ L01-gradle/src/main/java/ru/otus/App.java
@@ -5,6 +5,7 @@ import com.google.common.base.Ticker;
 public class App {
     public static void main(String[] args) {
         Ticker systemTicker = Ticker.systemTicker();
+
         System.out.println(systemTicker.read());
     }
 }
