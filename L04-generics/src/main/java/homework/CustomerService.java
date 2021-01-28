fcf8661a03bac675a173ed9bf0bfbfa4b087eb11
Aleksei Terentev <aleksei.terentev@NetCracker.com>
2021-01-28 21:31:06 +0400

    hw04-generics

--- L04-generics/src/main/java/homework/CustomerService.java
+++ L04-generics/src/main/java/homework/CustomerService.java
@@ -2,18 +2,17 @@ package homework;
 
 
 import java.util.AbstractMap;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Objects;
+import java.util.NavigableMap;
 import java.util.TreeMap;
 
 public class CustomerService {
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
-    private final TreeMap<Customer, String> data = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+    private final NavigableMap<Customer, String> data = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
