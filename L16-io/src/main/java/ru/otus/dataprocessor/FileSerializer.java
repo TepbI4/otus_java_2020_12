a92dca86190e5385b39a0257a484068719899c70
Aleksei Terentev <aleksei.terentev@NetCracker.com>
2021-05-10 17:20:53 +0400

    hw16-io

--- L16-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ L16-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,34 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class FileSerializer implements Serializer {
 
+    Gson gson;
+
+    Path path;
+
     public FileSerializer(String fileName) {
+        this.gson = new Gson();
+        this.path = Paths.get(fileName);
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
         //формирует результирующий json и сохраняет его в файл
+        var dataMap = new TreeMap<>(data);
+        var json = gson.toJson(dataMap);
+        try {
+            Files.writeString(path, json);
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
